5a1f0d394bdbb02760ec48f8340d4dc30b0cf8e8
Evan Huus <eapache@gmail.com>
2014-08-31 10:26:36 -0400

    Two more tests, just for completeness's sake

--- queue_test.go
+++ queue_test.go
@@ -2,6 +2,39 @@ package queue
 
 import "testing"
 
+func TestQueueSimple(t *testing.T) {
+	q := New()
+
+	for i := 0; i < minQueueLen; i++ {
+		q.Add(i)
+	}
+	for i := 0; i < minQueueLen; i++ {
+		if q.Peek().(int) != i {
+			t.Error("peek", i, "had value", q.Peek())
+		}
+		q.Remove()
+	}
+}
+
+func TestQueueWrapping(t *testing.T) {
+	q := New()
+
+	for i := 0; i < minQueueLen; i++ {
+		q.Add(i)
+	}
+	for i := 0; i < 3; i++ {
+		q.Remove()
+		q.Add(minQueueLen + i)
+	}
+
+	for i := 0; i < minQueueLen; i++ {
+		if q.Peek().(int) != i+3 {
+			t.Error("peek", i, "had value", q.Peek())
+		}
+		q.Remove()
+	}
+}
+
 func TestQueueLength(t *testing.T) {
 	q := New()
 
